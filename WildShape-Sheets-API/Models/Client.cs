1075e75f05796cec51f4f936f8f9385e39924ba5
Mark Ricard <34170972+Dragonfrut@users.noreply.github.com>
2023-05-27 17:52:56 -0400

    inital setup

---
+++ WildShape-Sheets-API/Models/Client.cs
@@ -0,0 +1,24 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace WildShape_Sheets_API.Models {
+    public class Client {
+
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public Guid Id { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Username { get; set; }
+        public string? Password { get; set; }
+        public string? Email { get; set; }
+        public DateTime RegistrationTime { get; set; }
+
+        public Client()
+        {
+            
+        }
+
+
+    }
+}
---
+++ WildShape-Sheets-API/Services/ClientService.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+using WildShape_Sheets_API.Models;
+
+namespace WildShape_Sheets_API.Services {
+    public class ClientService {
+        private readonly IMongoCollection<Client> _clientCollection;
+
+        public ClientService(IOptions<ClientDatabaseSetings> clientDatabaseSettings) {
+            var mongoClient = new MongoClient(
+                clientDatabaseSettings.Value.ConnectionString);
+
+            var mongoDatabase = mongoClient.GetDatabase(
+            clientDatabaseSettings.Value.DatabaseName);
+
+            _clientCollection = mongoDatabase.GetCollection<Client>(
+                clientDatabaseSettings.Value.BooksCollectionName);
+        }
+
+        public async Task<List<Client>> GetAsync() =>
+            await _clientCollection.Find(_ => true).ToListAsync();
+
+
+        
+    }
+}
