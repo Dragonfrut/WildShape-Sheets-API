450f60040aa8eb818cbf4c3a0c58a7955f4425a2
Erik <notnotbige@gmail.com>
2023-07-23 09:46:28 -0700

    email serivce work in progress

--- WildShape-Sheets-API/Controllers/UserController.cs
+++ WildShape-Sheets-API/Controllers/UserController.cs
@@ -11,9 +11,13 @@ namespace WildShape_Sheets_API.Controllers {
     public class UserController : Controller {
 
         private readonly UserService userService;
+        private readonly EmailService emailService;
 
-        public UserController(UserService _userService) =>
+        public UserController(UserService _userService, EmailService _emailService)
+        {
             userService = _userService;
+            emailService = _emailService;
+        }
 
         [HttpGet]
         public ActionResult<List<User>> GetUsers() {
@@ -38,5 +42,41 @@ namespace WildShape_Sheets_API.Controllers {
             userService.DeleteUser(id);
         }
 
+        public class PasswordResetRequest
+        {
+            public string email { get; set; }
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        [Route("password/reset")]
+        public ActionResult PasswordReset([FromBody] PasswordResetRequest passwordReset)
+        {
+
+            // Validate the emailForm, e.g., using ModelState.IsValid if needed
+            /*
+            if (string.IsNullOrEmpty(email))
+            {
+                return BadRequest(new { message = "Email is missing or empty." });
+            }
+            */
+            var user = userService.GetUserByEmail(passwordReset.email);
+            if (user == null)
+            {
+                return BadRequest(new { message = "Email not found" });
+            }
+            else
+            {
+                // Prepare the email subject and body
+                string subject = "Password Reset Request";
+                string body = "Hello password reset. This is the email body.";
+
+                // Send the email using the email service
+                emailService.SendPasswordResetEmail(passwordReset.email, subject, body);
+
+                return Ok();
+            }
+            return Ok();
         }
     }
+}
